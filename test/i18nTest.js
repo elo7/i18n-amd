67a1da5d3ae23217ef924c8b6d68bfbe1afad99f
Erika Takahara <login+erikatakahara@elo7.com>
2017-03-28 16:12:29 -0300

    Fix bug when using args and count on different domain

--- test/i18nTest.js
+++ test/i18nTest.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var vm = require('vm');
-var assert = require("assert");
+var assert = require('assert');
 
-var EXTERNAL_DOMAIN = 'http://localhost:3000';
+var EXTERNAL_DOMAIN = 'http://localhost:3030';
+var DEFAULT_DOMAIN = 'http://localhost:3000';
 
 vm.runInThisContext(fs.readFileSync('bower_components/async-define/async-define.js'));
 vm.runInThisContext(fs.readFileSync('i18n.js'));
@@ -11,13 +12,14 @@ vm.runInThisContext(
 	define('ajax',[], function() {
 		return {
 			get: function(url, data, callback, config) {
-				var key = url.replace('/i18n/', ''),
-						key = key.replace(EXTERNAL_DOMAIN, '');
-				if(messages && messages[key]) {
+				var args = url.split('/i18n/');
+				var domain = args[0] || DEFAULT_DOMAIN,
+					key = args[1];
+				if(messages[domain] && messages[domain][key]) {
 					var response = {
 						version: 1,
 						key: key,
-						message: messages[key]
+						message: messages[domain][key]
 					};
 					callback.success(response);
 				}
@@ -27,7 +29,9 @@ vm.runInThisContext(
 );
 
 function setup() {
-	session = {}, local = {}, messages = {};
+	messages = {};
+
+	session = {}, local = {};
 	sessionStorage = {
 		setItem: function(key, value) {
 			session[key] = value;
@@ -46,69 +50,74 @@ function setup() {
 	};
 }
 
+function addMessage(key, message, domain = DEFAULT_DOMAIN) {
+	messages[domain] = messages[domain] || {};
+	messages[domain][key] = message;
+}
+
 define(['i18n'], function(subject) {
 	describe('I18n', function() {
 		beforeEach(setup);
 
 		it('should return internacionalized string', function() {
-			messages['test.Title'] = 'Test';
+			addMessage('test.Title', 'Test');
 			assert.equal('Test', subject.get('test.Title'));
 		});
 
 		it('should update internacionalized string if theres a new version', function() {
 			// Cached value
-			messages['test.Title'] = 'Test';
+			addMessage('test.Title', 'Test');
 			assert.equal('Test', subject.get('test.Title'));
 
 			// Update cached value
 			sessionStorage.setItem('version', 2);
-			messages['test.Title'] = 'New Test';
+			addMessage('test.Title', 'New Test');
 			assert.equal('New Test', subject.get('test.Title'));
 		});
 
 		it('should not update internacionalized string if its the same version', function() {
-			messages['test.Title'] = 'Test';
+			addMessage('test.Title', 'Test');
 			assert.equal('Test', subject.get('test.Title'));
-			messages['test.Title'] = 'New Test';
+			addMessage('test.Title', 'New Test');
 			assert.equal('Test', subject.get('test.Title'));
 		});
 
 		it('should pluralize string when using count', function() {
-			messages['products.zero'] = 'No product';
-			messages['products.one'] = '1 product';
-			messages['products.other'] = '{0} products';
+			addMessage('products.zero', 'No product');
+			addMessage('products.one', '1 product');
+			addMessage('products.other', '{0} products');
 			assert.equal('No product', subject.count('products', 0));
 			assert.equal('1 product', subject.count('products', 1));
 			assert.equal('2 products', subject.count('products', 2));
 		});
 
 		it('should replace message arguments', function() {
-			messages['arguments.test'] = 'Hello {0}, you are {1}!';
+			addMessage('arguments.test', 'Hello {0}, you are {1}!');
 			assert.equal('Hello Bielo, you are awesome!', subject.args('arguments.test', 'Bielo', 'awesome'));
 		});
 
 		it('should not replace message arguments if theres no additional parameter', function() {
-			messages['arguments.test'] = 'Hello {0}, you are {1}!';
+			addMessage('arguments.test', 'Hello {0}, you are {1}!');
 			assert.equal('Hello {0}, you are {1}!', subject.args('arguments.test'));
 		});
 
 		it('should return internationalized string using a external domain', function() {
-			subject.domain = EXTERNAL_DOMAIN;
-			messages['test.Title'] = 'Test domain';
+			subject.domain(EXTERNAL_DOMAIN);
+			addMessage('test.Title', 'Test domain', EXTERNAL_DOMAIN);
 			assert.equal('Test domain', subject.get('test.Title'));
 		});
 
 		it('should replace message arguments using a external domain', function() {
-			subject.domain = EXTERNAL_DOMAIN;
-			messages['arguments.test'] = 'Hello {0}, you are {1}!';
+			subject.domain(EXTERNAL_DOMAIN);
+			addMessage('arguments.test', 'Hello {0}, you are {1}!', EXTERNAL_DOMAIN);
 			assert.equal('Hello Bielo, you are awesome!', subject.args('arguments.test', 'Bielo', 'awesome'));
 		});
 
 		it('should pluralize string when using count using a external domain', function() {
-			subject.domain = EXTERNAL_DOMAIN;
-			messages['products.zero'] = 'No product';
-			messages['products.one'] = '1 product';
-			messages['products.other'] = '{0} products';
+			subject.domain(EXTERNAL_DOMAIN);
+			addMessage('products.zero', 'No product', EXTERNAL_DOMAIN);
+			addMessage('products.one', '1 product', EXTERNAL_DOMAIN);
+			addMessage('products.other', '{0} products', EXTERNAL_DOMAIN);
 			assert.equal('No product', subject.count('products', 0));
 			assert.equal('1 product', subject.count('products', 1));
 			assert.equal('2 products', subject.count('products', 2));
